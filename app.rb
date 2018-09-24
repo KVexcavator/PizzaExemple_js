82548844abe2e93c2bb2882505b67468dab95d3d
KVexcavator <str_token@mail.ru>
2018-09-24 18:19:05 +0600

    set db, create untity Product, Rakefile,Gems

--- app.rb
+++ app.rb
@@ -1,6 +1,13 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+set :database, 'sqlite3:pizza.bd'
+
+class Product < ActiveRecord::Base
+end
+
 
 get '/' do
 	erb :index			
