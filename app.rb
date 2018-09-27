8c925e08c1c6e638596985b684bfb1af5a5e7ecb
KVexcavator <str_token@mail.ru>
2018-09-27 19:41:22 +0600

    add orders_parse_lline function

--- app.rb
+++ app.rb
@@ -21,3 +21,17 @@ end
 post '/cart' do
 	erb "cart post test"			
 end
+
+def parse_orders_line orders_line
+			s1=orders_line.split(',')
+			arr=[]
+			s1.each do |s|
+				s2=s.split('=')
+				s3=s2[0].split('_')
+				id=s3[1]
+				cnt=s2[1]	
+				arr_int=[id,cnt]
+				arr.push arr_int
+			end
+			return arr
+end
